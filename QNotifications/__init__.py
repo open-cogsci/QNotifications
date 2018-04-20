ffd3707cb847872f398aa3f9fa1d25e2c82bdefd
Sebastiaan Mathot <s.mathot@cogsci.nl>
2018-04-20 12:10:51 +0200

    Bump to 2.0.1

--- QNotifications/__init__.py
+++ QNotifications/__init__.py
@@ -17,7 +17,7 @@ GNU General Public License for more details.
 You should have received a copy of the GPLv3 License
 along with this module.>.
 """
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 __author__ = "Daniel Schreij (dschreij@gmail.com)"
 
 # Do some base imports
